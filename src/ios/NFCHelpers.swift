54f736504c1a221894edc649754432e0217a2245
GVercasson <geraud.vercasson@iotize.com>
2025-11-21 14:09:55 +0100

    fix memory issues: safe HexString to bytes conversion

--- src/ios/NFCHelpers.swift
+++ src/ios/NFCHelpers.swift
@@ -1,18 +1,21 @@
 import CoreNFC
 
 public extension String {
-    
-     func dataFromHexString() -> Data {
+    func dataFromHexString() -> Data {
         var bytes = [UInt8]()
-        for i in 0..<(count/2) {
-            let range = index(self.startIndex, offsetBy: 2*i)..<index(self.startIndex, offsetBy: 2*i+2)
-            let stringBytes = self[range]
-            let byte = strtol((stringBytes as NSString).utf8String, nil, 16)
-            bytes.append(UInt8(byte))
+        bytes.reserveCapacity(count / 2)
+        
+        var index = startIndex
+        while index < endIndex {
+            let next = self.index(index, offsetBy: 2)
+            let byteString = String(self[index..<next])
+            let byte = UInt8(byteString, radix: 16)!
+            bytes.append(byte)
+            index = next
         }
-        return Data(bytes: UnsafePointer<UInt8>(bytes), count:bytes.count)
+
+        return Data(bytes)
     }
-    
 }
 
 public extension Data {
